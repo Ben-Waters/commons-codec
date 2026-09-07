2cef7901851895712679d58c8d6237491050b2ac
Ben Waters <benjamin.waters@outlook.com>
2026-09-07 17:21:14 -0400

    CODEC-308 : add test case for trailing S to avoid empty string

--- src/test/java/org/apache/commons/codec/language/NysiisTest.java
+++ src/test/java/org/apache/commons/codec/language/NysiisTest.java
@@ -17,11 +17,11 @@
 
 package org.apache.commons.codec.language;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.apache.commons.codec.AbstractStringEncoderTest;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests {@link Nysiis}
@@ -137,7 +137,8 @@ class NysiisTest extends AbstractStringEncoderTest<Nysiis> {
                 // violates 6: if the last two characters are AY, remove A
                 new String[] { "CARRAWAY", "CARY" },       // Original: CARAY
                 new String[] { "YAMADA", "YANAD" },
-                new String[] { "ASH", "A"});
+                new String[] { "ASH", "A"},
+                new String[] { "SSH", "S"});
     }
 
     @Test
